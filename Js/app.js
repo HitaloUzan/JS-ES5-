67bed4c7737789b066e95a666b3a9816a1fa4bac
HitaloUzan <hitalouzan@gmail.com>
2024-03-05 16:02:22 -0300

    Aula 1 Variáveis e operadores

---
+++ Js/app.js
@@ -0,0 +1,15 @@
+// Console.log("olá mundo!!")
+
+let nome = "Hitalo"; // String
+let sobrenome = "Uzan";
+let string = `o ${nome} tem o sobrenome ${sobrenome}`;
+let idade = 25; // Number
+let feliz = true; //Boolean
+let nada; //undefined
+let simbolo = Symbol();
+let object = {}; // objeto
+
+var soma = 200 + 200;
+
+console.log(string);
+console.log(soma);
